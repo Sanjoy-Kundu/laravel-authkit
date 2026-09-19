ec30da131104acf421287f5d87312b12c0ed9b7e
unknown <sanjoykundu187@gmail.com>
2026-09-19 17:11:52 +0600

    laravel authkit project folder has been added done

---
+++ packages/sanjoy/laravel-authkit/src/AuthKitServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sanjoy\AuthKit;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthKitServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
